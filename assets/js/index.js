b648eb9df44b98c61bff8b2a603c574b46130832
crittycall <charles.robinson1012@icloud.com>
2025-08-31 21:13:15 -0400

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,8 @@ const titles = [
     "https://media.discordapp.net/attachments/1251639179658526825/1410792216007278764/IMG_2251.png?ex=68b2f6e3&is=68b1a563&hm=ce46d03dfa841885de87ebf377ee50cdd59803140422d215ae17b9ef6da9ae32&=&width=1037&height=1008"
 ]
 
-function ChangeImage() {
-    const randomimage = Math.floor(Math.random() * titles.length)
-    console.log(randomimage)
-    document.getElementById('clicktitle').src = titles[randomimage]
-
+function randomimage() {
+    const randomimagegen = Math.floor(Math.random * titles.length)
+    const element = document.getElementById('yourid')
+    element.src = randomimagegen
 }
